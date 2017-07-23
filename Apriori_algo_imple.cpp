44e653700877f998a9286c676755db283c9886f6
arif <arifkhanshubro@gmail.com>
2017-07-23 22:56:20 +0600

    Implementation of Apriori Algorithm in C++

--- Apriori_algo_imple.cpp
+++ Apriori_algo_imple.cpp
@@ -1,7 +1,43 @@
 #include<bits/stdc++.h>
+using namespace std;
 
 int main()
 {
+    //ios_base::sync_with_stdio(false);cin.tie(NULL);
+    freopen("input.txt","r",stdin);
+
+    int transaction,maximum_purchase_item,minimum_support;
+
+   // cout << "Enter Transaction Number : " ;
+    cin >> transaction ;
+    //cout << endl << "Enter Maximum Purchase Item : " ;
+    cin >> maximum_purchase_item;
+    //cout << endl;
+    //cout << "Enter Minimum Support : " ;
+    cin >> minimum_support;
+    //cout << endl;
+
+    cout << "Transaction Number : " << transaction << endl << "Maximum Purchase Item : " << maximum_purchase_item << endl << "Minimum Support : " << minimum_support << endl << endl;
+
+    string purchase_data[10][15];
+    for(int i = 0 ; i < transaction ; i++){
+        //cout << "Enter Transaction Id :" ;
+        cin >> purchase_data[i][0];
+        //cout << endl;
+        //cout << "Enter Purchase Item :" ;
+            for(int k = 1; k <= maximum_purchase_item ; k++){
+                cin >> purchase_data[i][k];
+            }
+            //cout << endl;
+    }
+    cout << "Purchase Data : " << endl;
+        for(int i = 0 ; i < transaction ; i++){
+          cout << purchase_data[i][0] << " : ";
+            for(int k = 1; k <= maximum_purchase_item ; k++){
+                cout << " " << purchase_data[i][k];
+            }
+            cout << endl;
+    }
 
     return 0;
 }
